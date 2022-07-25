77560caf4ceafced4721f38e44d1068d426a0bcc
Hayri BAYIK <hyr.byk@gmail.com>
2022-07-25 16:14:05 +0300

    tamamlandı.

---
+++ BolumleriBulma/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BolumleriBulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi=0;
+            Console.WriteLine("Bölenlerini bulmak istediğiniz sayıyı giriniz");
+            sayi=Convert.ToInt32(Console.ReadLine());
+           for (int i = 1; i <= sayi; i++)
+			{
+                if (sayi%i==0)
+	            {
+                    Console.WriteLine(i);
+	            }
+			}
+
+           Console.ReadKey();
+        }
+    }
+}
